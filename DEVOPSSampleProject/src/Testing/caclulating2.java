14ad4de9887115a478643c285e788dd4b053b584
hrushi5050 <hrishikeshjadhav50@gmail.com>
2019-06-23 16:22:36 +0530

    {1234}adding calc

---
+++ DEVOPSSampleProject/src/Testing/caclulating2.java
@@ -0,0 +1,29 @@
+package Testing;
+
+import org.junit.Test;
+
+public class caclulating2 {
+	
+	int a=1000;
+	int b=200;
+	@Test
+	public void sum()
+	
+	{
+		System.out.println(a+b);
+	}
+	
+	@Test
+	public void multi()
+	
+	{
+		System.out.println(a*b);
+	}
+	@Test
+	public void diff()
+	
+	{
+		System.out.println(a-b);
+	}
+	
+}
---
+++ DEVOPSSampleProject/src/Testing/calculate.java
@@ -0,0 +1,14 @@
+package Testing;
+
+public class calculate {
+	public static void main (String[] args){
+		
+		int x=2;
+			int y=3;
+		
+		int sum=x+y;
+		
+		System.out.println(sum);
+	}
+
+}
